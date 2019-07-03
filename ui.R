42fbdfbb6016c6a005032fad91efa6465a576e4b
Chris Pilling <christopher.pilling@uk.fujitsu.com>
2019-07-03 19:06:27 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -7,22 +7,22 @@ shinyUI(fluidPage(
     tags$link(rel="stylesheet", type="text/css", href="custom_styles.css")
   ),
   
-  title = "Sam travels America",
+  title = "Traveling Salesman with Simulated Annealing, Shiny, and R",
   
-tags$h2(tags$a(href="", "Create Sam's tour route", target="_blank")),
+  tags$h2(tags$a(href="/traveling-salesman", "Traveling Salesman", target="_blank")),
   
   plotOutput("map", height="550px"),
   
   fluidRow(
     column(5,
       tags$ol(
-        tags$li("Pick the tour you want Sam to travel"),
-        tags$li("Adjust simulated annealing parameters"),
-        tags$li("Click the 'Route Me' button!")
+        tags$li("Customize the list of cities, based on the world or US map"),
+        tags$li("Adjust simulated annealing parameters to taste"),
+        tags$li("Click the 'solve' button!")
       )
     ),
     column(3,
-      tags$button("Route Me", id="go_button", class="btn btn-info btn-large action-button shiny-bound-input")
+      tags$button("SOLVE", id="go_button", class="btn btn-info btn-large action-button shiny-bound-input")
     ),
     column(3,
       HTML("<button id='set_random_cities_2' class='btn btn-large action-button shiny-bound-input'>
@@ -37,7 +37,7 @@ tags$h2(tags$a(href="", "Create Sam's tour route", target="_blank")),
   fluidRow(
     column(5,
       h4("Choose a map and which cities to tour"),
-      selectInput("map_name", NA, c("USA"), width="100px"),
+      selectInput("map_name", NA, c("World", "USA"), "World", width="100px"),
       p("Type below to select individual cities, or", actionButton("set_random_cities", "set randomly", icon=icon("refresh"))),
       selectizeInput("cities", NA, all_cities$full.name, multiple=TRUE, width="100%",
                      options = list(maxItems=30, maxOptions=100, placeholder="Start typing to select some cities...",
