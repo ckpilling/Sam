acb774d421d3474ad710457569eb247d7c406366
Chris Pilling <christopher.pilling@uk.fujitsu.com>
2019-07-03 20:24:58 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -22,9 +22,14 @@ shinyUI(fluidPage(
       )
     ),
     column(3,
-      tags$button("SOLVE", id="go_button", class="btn btn-info btn-large action-button shiny-bound-input")
-    ),
-),
+      HTML("<button id='set_random_cities_2' class='btn btn-large action-button shiny-bound-input'>
+              <i class='fa fa-refresh'></i>
+              Set Cities Randomly
+            </button>")
+    ), class="aaa"
+  ),
+  
+  hr(),
   
   fluidRow(
     column(5,
