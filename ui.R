b55d6591336ac33472162434a541690eb7a79c38
Chris Pilling <christopher.pilling@uk.fujitsu.com>
2019-07-03 19:13:29 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -37,7 +37,7 @@ shinyUI(fluidPage(
   fluidRow(
     column(5,
       h4("Choose cities to tour"),
-      selectInput("map_name", NA, c("USA"), "", width="100px"),
+      selectInput("map_name", NA, c("USA",""), "", width="100px"),
       p("Type below to select individual cities, or", actionButton("set_random_cities", "set randomly", icon=icon("refresh"))),
       selectizeInput("cities", NA, all_cities$full.name, multiple=TRUE, width="100%",
                      options = list(maxItems=30, maxOptions=100, placeholder="Start typing to select some cities...",
