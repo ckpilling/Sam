801555ae2377ba117d918314b4e9920839e043d5
Chris Pilling <christopher.pilling@uk.fujitsu.com>
2019-07-03 21:26:01 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -25,8 +25,8 @@ shinyUI(fluidPage(
       tags$button("Route Me", id="go_button", class="btn btn-info btn-large action-button shiny-bound-input")
     ),
     column(3,
-      HTML("<p>
-            </p")
+      HTML("<button id='set_random_cities_2' class='btn btn-large'>
+            </button>")
     ), class="aaa"
   ),
   
