df86a61bac1cb9157cadf636dec693883f490963
Chris Pilling <christopher.pilling@uk.fujitsu.com>
2019-07-03 23:01:02 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -21,10 +21,10 @@ shinyUI(fluidPage(
         tags$li("Click the 'Optimise Now!' button!")
       )
     ),
-    column(5,
+    column(3,
       tags$button("Optimise Now!", id="go_button", class="btn btn-info btn-large action-button shiny-bound-input")
     ),
-      column(5,
+      column(3,
       HTML("<button id='set_random_cities_2' class='btn btn-small action-button shiny-bound-input' style='background-color:white;color:white'>
             </button>")
     ), class="aaa"
