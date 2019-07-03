4e61e513c0c771297819d21a9ff505467ed84cdb
Chris Pilling <christopher.pilling@uk.fujitsu.com>
2019-07-03 20:43:40 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -23,7 +23,7 @@ shinyUI(fluidPage(
     ),
     column(3,
       tags$button("Route Me", id="go_button", class="btn btn-info btn-large action-button shiny-bound-input")
-    ), 
+    )
     class="aaa"
   ),
   
