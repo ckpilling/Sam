171d99ef6b26b8beccbc5fd9460bad768d9ba656
Chris Pilling <christopher.pilling@uk.fujitsu.com>
2019-07-04 07:19:59 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -34,7 +34,7 @@ shinyUI(fluidPage(
   
   fluidRow(
     column(5,
-      p("Type the cities for Sam's gigs, or", actionButton("set_random_cities", "pick random cities", icon=icon("refresh"))),
+      p("Type the cities for Sam's gigs, or", actionButton("set_random_cities", "auto pick cities", icon=icon("refresh"))),
       selectizeInput("cities", NA, all_cities$full.name, multiple=TRUE, width="100%",
                      options = list(maxItems=30, maxOptions=100, placeholder="Start typing to select some cities...",
                                     selectOnTab=TRUE, openOnFocus=FALSE, hideSelected=TRUE)),
